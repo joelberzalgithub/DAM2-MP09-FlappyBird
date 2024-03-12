814cb54a833707b442f71c9cc6c752d3e1818c53
achivaq01 <achicavaquerizo.cf@iesesteveterradas.cat>
2024-03-12 18:43:34 +0100

    added birds

--- lib2/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flame/game.dart';
-import 'package:flutter/material.dart';
-
-import 'game.dart';
-
-void main() {
-  final game = FlappyEmber();
-  runApp(GameWidget(game: game));
-}
