9ed0a7ea8618f54198e88d2947397e2bd23e55bb
achivaq01 <145363069+achivaq01@users.noreply.github.com>
2024-04-28 22:00:01 +0200

    added log folder

--- server/src/logger/logger.js
+++ server/src/logger/logger.js
@@ -28,8 +28,8 @@ const logger = winston.createLogger({
     ),
     defaultMeta: { service: 'user-service' },
     transports: [
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' }),
+      new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
+      new winston.transports.File({ filename: './logs/combined.log' }),
       new winston.transports.Console(),
     ],
   });
